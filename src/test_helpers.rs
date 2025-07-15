623a8dc6175f824d2527e63916aea337fedbea72
Pierre Barre <pierre@barre.sh>
2025-07-15 13:32:50 +0200

    Add test authentication helper

--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -1,8 +1,17 @@
 #[cfg(test)]
 pub mod test_helpers {
     use nfsserve::nfs::nfsstring;
+    use nfsserve::vfs::AuthContext;
 
     pub fn filename(s: &[u8]) -> nfsstring {
         nfsstring(s.to_vec())
+    }
+
+    pub fn test_auth() -> AuthContext {
+        AuthContext {
+            uid: 1000,
+            gid: 1000,
+            gids: vec![],
+        }
     }
 }
